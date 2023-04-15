59a2290b47478ca9938ff57fa9ff2ab89b576460
SULTAN PEYEK <sultanpeyek999@gmail.com>
2023-04-15 12:14:24 +0700

    update

--- src/env.mjs
+++ src/env.mjs
@@ -14,7 +14,11 @@ const server = z.object({
   NEXTAUTH_URL: z.preprocess(
     // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
     // Since NextAuth.js automatically uses the VERCEL_URL if present.
-    (str) => process.env.VERCEL_URL ?? str,
+    (str) => {
+      console.log(str);
+      console.log(process.env.VERCEL_URL);
+      return process.env.VERCEL_URL ?? str;
+    },
     // VERCEL_URL doesn't include `https` so it cant be validated as a URL
     process.env.VERCEL ? z.string().min(1) : z.string().url()
   ),
